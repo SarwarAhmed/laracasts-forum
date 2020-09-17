58fb105015241c09ccf3f78b68e67c22043db958
Sarwar Ahmed <sarwarahmed6@gmail.com>
2020-09-17 14:38:37 +0600

    E-5: The Reply Forum

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,23 @@
             @endforeach
         </div>
     </div>
+    
+    @auth
+        <div class="row justify-content-center mt-4">
+            <div class="col-md-8">
+                <form method="POST" action="{{ $thread->path() . '/replies' }}">
+                    @csrf
+
+                    <div class="form">
+                        <textarea name="body" id="body" class="form-control" placeholder="Have something to say?" rows="5"></textarea>
+                    </div>
+
+                    <button type="submit" class="mt-2 btn btn-outline-primary">Post</button>
+                </form>
+            </div>
+        </div>
+    @else 
+        <p class="text-center pt-2">Please <a href="{{ route('login') }}">Sign in</a> to participate to this discussion.</p>
+    @endauth
 </div>
 @endsection
