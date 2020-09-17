81c9128a5685fe00ee5b879c051f80a52b3c7f3d
Sarwar Ahmed <sarwarahmed6@gmail.com>
2020-09-17 15:23:25 +0600

    E-6: A User Can Publish Threads

--- routes/web.php
+++ routes/web.php
@@ -24,6 +24,7 @@ Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
 Route::get('/threads', [ThreadsController::class, 'index']);
+Route::post('/threads', [ThreadsController::class, 'store']);
 Route::get('/threads/{thread}', [ThreadsController::class, 'show']);
 
 Route::post('/threads/{thread}/replies', [RepliesController::class, 'store']);
---
+++ tests/Feature/CreateThreadsTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use App\Models\User;
+use App\Models\Thread;
+use Illuminate\Auth\AuthenticationException;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class CreateThreadsTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+    public function guest_may_not_create_threads()
+    {
+        $this->withoutExceptionHandling();
+        
+        $this->expectException(AuthenticationException::class);
+
+        $thread = Thread::factory()->create();
+
+        $this->post('/threads', $thread->toArray());
+    }
+
+    /** @test */
+    public function an_authenticated_uesr_can_create_new_forum_threads()
+    {
+        $this->withoutExceptionHandling();
+
+        $this->actingAs(User::factory()->create());
+
+        $thread = Thread::factory()->make();
+
+        $this->post('/threads', $thread->toArray());
+
+        $this->get($thread->path())
+            ->assertSee($thread->title)
+            ->assertSee($thread->body);
+    }
+}
