3939032434ef1e00348f1677fc072912ac199b0d
sarwar ahmed <sarwar.amas@gmail.com>
2020-09-18 00:00:17 +0600

    E-12: Validation Errors and Old Data

--- resources/views/threads/create.blade.php
+++ resources/views/threads/create.blade.php
@@ -12,16 +12,43 @@
                         @csrf
 
                         <div class="form-group">
+                            <label for="channel_id">Choose a Channel:</label>
+                            <select name="channel_id" id="channel_id" class="form-control">
+
+                                <option value="" selected disabled>Choose One..</option>
+
+                                @foreach (App\Models\Channel::all() as $channel)
+                                    <option 
+                                        value="{{ $channel->id }}" 
+                                        {{ old('channel_id') == $channel->id ? 'selected' : '' }}
+                                    >
+                                        {{ $channel->name }}
+                                    </option>
+                                @endforeach
+                            </select>
+                        </div>
+
+                        <div class="form-group">
                             <label for="title">Title</label>
-                            <input type="text" name="title" class="form-control" id="title">
+                            <input type="text" name="title" class="form-control" id="title" value="{{ old('title') }}">
                         </div>
                         
                         <div class="form-group">
                             <label for="body">Description</label>
-                            <textarea name="body" id="body" class="form-control" rows="6"></textarea>
+                            <textarea name="body" id="body" class="form-control" rows="6">{{ old('body') }}</textarea>
                         </div>
 
                         <button type="submit" class="mt-2 btn btn-outline-primary">Publish</button>
+
+                        @if ($errors->any())
+                            <div class="alert alert-danger mt-2">
+                                <ul>
+                                    @foreach ($errors->all() as $error)
+                                        <li>{{ $error }}</li>
+                                    @endforeach
+                                </ul>
+                            </div>
+                        @endif
                     </form>
                </div>
             </div>
