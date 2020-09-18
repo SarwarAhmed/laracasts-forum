df8fb9f42ac409a6a525d7d629a8e62f2e6bf99c
sarwar ahmed <sarwar.amas@gmail.com>
2020-09-18 22:18:44 +0600

    E-20: From 56 Queries Down to 2

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,11 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         \View::composer('*', function ($view) {
-            $view->with('channels', Channel::all());
+            $channels = \Cache::rememberForever('channels', function () {
+                return Channel::all();
+            });
+
+            $view->with('channels', $channels);
         });
 
         // getBootstrap css for pagination
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -14,8 +14,8 @@
                     <button class="btn btn-secondary btn-sm"
                         {{ $reply->isFavorited() ? 'disabled' : '' }}
                     >
-                        {{ $reply->favorites()-> count() }}
-                        {{ Str::plural('Favorite', $reply->favorites()->count()) }}
+                        {{ $reply->favorites_count }}
+                        {{ Str::plural('Favorite', $reply->favorites_count) }}
                     </button>
                 </form>
             </div>
