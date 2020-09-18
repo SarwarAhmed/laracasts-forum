6a1cc8cd3bc9585de2a0df782b56607940f0f714
sarwar ahmed <sarwar.amas@gmail.com>
2020-09-18 21:27:29 +0600

    E-19: The Favorite Button

--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,25 @@
 <br>
 <div class="card">
     <div class="card-header">
-        <a href="#">{{ $reply->owner->name }}</a> 
-        said {{ $reply->created_at->diffForHumans() }}
+        <div class="input-group-append justify-content-between">
+            <h5>
+                <a href="#">{{ $reply->owner->name }}</a> 
+                said {{ $reply->created_at->diffForHumans() }}
+            </h5>
+
+            <div>
+                <form method="POST" action="/replies/{{ $reply->id }}/favorites">
+                    @csrf
+
+                    <button class="btn btn-secondary btn-sm"
+                        {{ $reply->isFavorited() ? 'disabled' : '' }}
+                    >
+                        {{ $reply->favorites()-> count() }}
+                        {{ Str::plural('Favorite', $reply->favorites()->count()) }}
+                    </button>
+                </form>
+            </div>
+        </div>
     </div>
     <div class="card-body">
         <p>{{ $reply->body }}</p>
