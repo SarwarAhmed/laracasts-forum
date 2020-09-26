b4eb4453d5b3883d5d00b2841013aff108293254
sarwar ahmed <sarwar.amas@gmail.com>
2020-09-26 19:39:08 +0600

    E-46: Refactoring for the Better or Worse?

--- app/Models/Thread.php
+++ app/Models/Thread.php
@@ -52,12 +52,17 @@ class Thread extends Model
     {
         $reply = $this->replies()->create($reply);
 
+        $this->notifySubscribers($reply);
+
+        return $reply;
+    }
+
+    public function notifySubscribers($reply)
+    {
         $this->subscriptions
             ->where('user_id', '!=', $reply->user_id)
             ->each
             ->notify($reply);
-
-        return $reply;
     }
 
     public function scopeFilter($query, ThreadFilters $filters)
