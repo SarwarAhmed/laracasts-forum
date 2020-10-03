6c831d38933b13657cac8cdb10510dbcd03e482f
sarwar ahmed <sarwar.amas@gmail.com>
2020-10-03 20:46:06 +0600

    E-59: Wrap Usernames Within Anchor Tags

--- resources/js/components/Reply.vue
+++ resources/js/components/Reply.vue
@@ -28,7 +28,7 @@
                     </form>
                 </div>
 
-                <div v-else v-text="body"></div>
+                <div v-else v-html="body"></div>
             </div>
 
             <div class="card-footer input-group-append" v-if="canUpdate">
--- tests/Unit/ReplyTest.php
+++ tests/Unit/ReplyTest.php
@@ -40,4 +40,17 @@ class ReplyTest extends TestCase
 
         $this->assertEquals(['JaneDoe', 'JohnDoe'], $reply->mentionedUsers());
     }
+
+    /** @test */
+    public function it_wraps_mentioned_usernames_in_the_body_within_anchor_tags()
+    {
+        $reply = new \App\Models\Reply ([
+            'body' => 'Hello @Jane-Doe.'
+        ]);
+
+        $this->assertEquals(
+            'Hello <a href="/profiles/Jane-Doe">@Jane-Doe</a>.',
+            $reply->body
+        );
+    }
 }
