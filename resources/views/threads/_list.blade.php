9174caf2e05638c5fc97906205d5a31b36a9770e
sarwar ahmed <sarwar.amas@gmail.com>
2020-10-05 20:52:11 +0600

    E-62: Basic View Tweaks

---
+++ resources/views/threads/_list.blade.php
@@ -0,0 +1,41 @@
+@forelse ($threads as $thread)
+    <div class="card mt-4 mb-5">
+        <div class="card-header">
+            <div class="input-group-append justify-content-between">
+                <div>
+                    <h4 class="mr-auto">
+                        <a href="{{ $thread->path() }}">
+                            @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+
+                                <strong>
+                                    {{ $thread->title }}
+                                </strong>
+                            @else
+                                {{ $thread->title }}
+                            @endif
+                        </a>
+                    </h4>
+
+                    <h5>
+                        Posted By: <a href="{{ route('profile', $thread->creator) }}">{{ $thread->creator->name }}</a>
+                    </h5>
+                </div>
+
+                <a href="{{ $thread->path() }}" class="">
+                    {{ $thread->replies_count }}
+                    {{ Str::plural('reply', $thread->replies_count) }}
+                </a>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <article>
+                <div class="body">{{ $thread->body }}</div>
+            </article>
+        </div>
+    </div>
+@empty
+    <div class="card">
+        <p class="card-header">Threre are no relavant results at this time.</p>
+    </div>
+@endforelse
--- resources/views/threads/index.blade.php
+++ resources/views/threads/index.blade.php
@@ -4,41 +4,9 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @forelse ($threads as $thread)
-                <div class="card mt-4">
-                    <div class="card-header">
-                        <div class="input-group-append justify-content-between">
-                            <h4 class="mr-auto">
-                                <a href="{{ $thread->path() }}">
-                                    @if (auth()->check() && $thread->hasUpdatesFor(auth()->user()))
+            @include('threads._list')
 
-                                        <strong>
-                                            {{ $thread->title }}
-                                        </strong>
-                                    @else
-                                        {{ $thread->title }}
-                                    @endif
-                                </a>
-                            </h4>
-
-                            <a href="{{ $thread->path() }}" class="">
-                                    {{ $thread->replies_count }}
-                                    {{ Str::plural('reply', $thread->replies_count) }}
-                            </a>
-                        </div>
-                    </div>
-
-                    <div class="card-body">
-                        <article>
-                            <div class="body">{{ $thread->body }}</div>
-                        </article>
-                    </div>
-                </div>
-            @empty
-                <div class="card">
-                    <p class="card-header">Threre are no relavant results at this time.</p>
-                </div>
-            @endforelse
+            <div>{{ $threads->appends(request()->input())->links() }}</div>
         </div>
     </div>
 </div>
