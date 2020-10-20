34e77b260ca4e93266a29aa104a6ab753015d622
sarwar ahmed <sarwar.amas@gmail.com>
2020-10-20 22:18:46 +0600

    E-93: A Thread Can Be Updated: Part 3

--- resources/views/threads/_question.blade.php
+++ resources/views/threads/_question.blade.php
@@ -3,22 +3,22 @@
     <div class="card-header">
         <div class="input-group-append justify-content-between">
 
-            <input type="text" class="form-control" value="{{ $thread->title }}">
+            <input type="text" class="form-control" v-model="form.title">
 
         </div>
     </div>
 
     <div class="card-body">
         <div class="form-group">
-            <textarea class="form-control" rows="10">{{ $thread->body }}</textarea>
+            <textarea class="form-control" rows="10" v-model="form.body"></textarea>
         </div>
     </div>
 
     <div class="card-footer input-group-append">
         <div>
             <button class="btn btn-sm btn-outline-secondary" @click="editing = true" v-show="! editing">Edit</button>
-            <button class="btn btn-sm btn-primary mr-2" @click="">Update</button>
-            <button class="btn btn-sm btn-link" @click="editing = false">Cancel</button>
+            <button class="btn btn-sm btn-primary mr-2" @click="update">Update</button>
+            <button class="btn btn-sm btn-link" @click="resetForm">Cancel</button>
         </div>
 
         @can ('update', $thread)
@@ -40,18 +40,23 @@
         <div class="input-group-append justify-content-between">
 
             <h5>
-                <img src="{{ $thread->creator->avatar_path }}" alt="{{ $thread->creator->name }}" width="25" height="25" class="mr-2">
-                <a href="{{ $thread->path() }}">{{ $thread->title }}</a>
+                <img src="{{ $thread->creator->avatar_path }}"
+                     alt="{{ $thread->creator->name }}"
+                     width="25"
+                     height="25"
+                     class="mr-2"
+                >
+                <span v-text="title"></span>
             </h5>
 
         </div>
     </div>
 
     <div class="card-body">
-        <p>{{ $thread->body }}</p>
+        <p v-text="body"></p>
     </div>
 
-    <div class="card-footer">
+    <div class="card-footer" v-if="authorize('owns', thread)">
         <button class="btn btn-sm btn-outline-secondary" @click="editing = true">Edit</button>
     </div>
 </div>
